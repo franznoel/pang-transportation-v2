181f9c69192f1a6785011960540ed461237f65fa
Franz Noel Tanglao <franz@asteriainteractive.com>
2016-07-19 17:54:39 -0700

    feat: Retrieve and serve static files from cache when offline.
    See: #5

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,8 @@
-self.addEventListener('fetch',function(event) {
+self.addEventListener('install',function(event) {
   console.log(event.request);
   caches.open('pang-transportation-static-v1').then(function(cache) {
     return cache.addAll([
+      '/',
       'libs/bootstrap/dist/css/bootstrap.css',
       'css/style.css',
       'libs/jquery/dist/jquery.min.js',
@@ -10,3 +11,17 @@ self.addEventListener('fetch',function(event) {
     ]);
   });
 });
+
+self.addEventListener('fetch',function(event) {
+  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      console.log(response);
+      if (!response) {
+        return fetch(event.request);
+      } else {
+        return response;
+      }
+    })
+  );
+})
